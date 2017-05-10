ec052628ec0dbaf35f7b5c1b3a80b511693b7fd5
Christophe Remacle <christophe_remacle@hotmail.com>
2017-05-10 14:29:29 +0200

    Fixed some litlle things

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,1 @@
-let gMap,
-	iAnimatedMarkers = 0;
-
-const DEFAULT_CENTER = new google.maps.LatLng( 50.67, 4.6 );
-const DEFAULT_ZOOM = 7;
-
-const fInitmap = function() {
-	gMap = new google.maps.Map( document.getElementById( "gmap" ), {
-		"center": DEFAULT_CENTER,
-		"mapTypeControl": false,
-		"scroollWheel": false,
-		"streetViewControl": false,
-		"zoom": DEFAULT_ZOOM,
-	} );
-};
+;(function(){"use strict";var gMap=void 0,iAnimatedMarkers=0;var DEFAULT_CENTER=new google.maps.LatLng(50.67,4.6);var DEFAULT_ZOOM=7;var fInitmap=function fInitmap(){gMap=new google.maps.Map(document.getElementById("gmap"),{"center":DEFAULT_CENTER,"mapTypeControl":false,"scroollWheel":false,"streetViewControl":false,"zoom":DEFAULT_ZOOM});};})();
